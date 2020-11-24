55dacdb1c3e36036408c998ff2ff3f7d57180f6d
Nikhil <nikhilmaske2001@gmail.com>
2020-11-24 17:51:03 +0530

    CHW PYTHON CH-2

---
+++ Demo/chapter2.py
@@ -0,0 +1,10 @@
+# Chapter 2 contains 
+# 1)Variables and datatypes
+# 2)Operators- Very similar to Javascript
+# 3)Typecasting
+# 4)Input function
+
+a = input("Enter value of a : ")
+print(type(a))
+a = int(a)
+print(type(a))
